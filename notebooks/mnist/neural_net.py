913f9ae526142820fa7d7eceb127bea67d5ee7f9
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-02-02 00:54:05 +0800

    refactor: reformat module

--- notebooks/mnist/neural_net.py
+++ notebooks/mnist/neural_net.py
@@ -26,16 +26,16 @@ class NeuralNet(tf.keras.Model):
     def __init__(self, **kwargs):
         super(NeuralNet, self).__init__()
         self.hidden_layer_1 = tf.keras.layers.Dense(
-            units=kwargs['units'][0],
-            activation=kwargs['activation'],
-            kernel_initializer=kwargs['initializer']
+            units=kwargs["units"][0],
+            activation=kwargs["activation"],
+            kernel_initializer=kwargs["initializer"],
         )
         self.hidden_layer_2 = tf.keras.layers.Dense(
-            units=kwargs['units'][1],
-            activation=kwargs['activation'],
-            kernel_initializer=kwargs['initializer']
+            units=kwargs["units"][1],
+            activation=kwargs["activation"],
+            kernel_initializer=kwargs["initializer"],
         )
-        self.output_layer = tf.keras.layers.Dense(units=kwargs['num_classes'])
+        self.output_layer = tf.keras.layers.Dense(units=kwargs["num_classes"])
         self.optimizer = tf.optimizers.SGD(learning_rate=3e-4, momentum=9e-1)
 
     @tf.function
@@ -67,7 +67,9 @@ def train(model, loss_fn, dataset, epochs):
         train_accuracy = []
         train_loss = 0
         for batch_features, batch_labels in dataset:
-            batch_features += tf.random.normal(stddev=(1. / (1. + epoch)**0.55), shape=batch_features.shape)
+            batch_features += tf.random.normal(
+                stddev=(1.0 / (1.0 + epoch) ** 0.55), shape=batch_features.shape
+            )
             loss = train_step(model, loss_fn, batch_features, batch_labels)
 
             accuracy = tf.metrics.Accuracy()
@@ -81,8 +83,12 @@ def train(model, loss_fn, dataset, epochs):
         epoch_accuracy.append(tf.reduce_mean(train_accuracy))
 
         if (epoch != 0) and ((epoch + 1) % 50 == 0):
-            print('epoch {}/{} : mean loss = {}, mean accuracy = {}'.format(epoch + 1,
-                                                                            epochs,
-                                                                            tf.reduce_mean(train_loss),
-                                                                            tf.reduce_mean(train_accuracy)))
+            print(
+                "epoch {}/{} : mean loss = {}, mean accuracy = {}".format(
+                    epoch + 1,
+                    epochs,
+                    tf.reduce_mean(train_loss),
+                    tf.reduce_mean(train_accuracy),
+                )
+            )
     return epoch_accuracy, epoch_loss
