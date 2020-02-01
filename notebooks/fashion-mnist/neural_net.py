ca5d23fca15e7245eb50d6b2ffb4c1330dbffefe
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-02-02 00:45:10 +0800

    chore: add train_step and train functions

--- notebooks/fashion-mnist/neural_net.py
+++ notebooks/fashion-mnist/neural_net.py
@@ -22,16 +22,16 @@ class NeuralNet(tf.keras.Model):
     def __init__(self, **kwargs):
         super(NeuralNet, self).__init__()
         self.hidden_layer_1 = tf.keras.layers.Dense(
-            units=kwargs['units'][0],
-            activation=kwargs['activation'],
-            kernel_initializer=kwargs['initializer']
+            units=kwargs["units"][0],
+            activation=kwargs["activation"],
+            kernel_initializer=kwargs["initializer"],
         )
         self.hidden_layer_2 = tf.keras.layers.Dense(
-            units=kwargs['units'][1],
-            activation=kwargs['activation'],
-            kernel_initializer=kwargs['initializer']
+            units=kwargs["units"][1],
+            activation=kwargs["activation"],
+            kernel_initializer=kwargs["initializer"],
         )
-        self.output_layer = tf.keras.layers.Dense(units=kwargs['num_classes'])
+        self.output_layer = tf.keras.layers.Dense(units=kwargs["num_classes"])
         self.optimizer = tf.optimizers.SGD(learning_rate=3e-4, momentum=9e-1)
 
     @tf.function
@@ -45,3 +45,46 @@ class NeuralNet(tf.keras.Model):
 def loss_fn(logits, labels):
     softmax_loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
     return tf.reduce_mean(softmax_loss)
+
+
+def train_step(model, loss, features, labels):
+    with tf.GradientTape() as tape:
+        logits = model(features)
+        train_loss = loss(logits=logits, labels=labels)
+    gradients = tape.gradient(train_loss, model.trainable_variables)
+    model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return train_loss
+
+
+def train(model, loss_fn, dataset, epochs):
+    epoch_accuracy = []
+    epoch_loss = []
+    for epoch in range(epochs):
+        train_accuracy = []
+        train_loss = 0
+        for batch_features, batch_labels in dataset:
+            batch_features += tf.random.normal(
+                stddev=(1.0 / (1.0 + epoch) ** 0.55), shape=batch_features.shape
+            )
+            loss = train_step(model, loss_fn, batch_features, batch_labels)
+
+            accuracy = tf.metrics.Accuracy()
+            predictions = tf.nn.softmax(model(batch_features))
+            accuracy(tf.argmax(predictions, 1), tf.argmax(batch_labels, 1))
+
+            train_loss += loss
+            train_accuracy.append(accuracy.result())
+
+        epoch_loss.append(tf.reduce_mean(train_loss))
+        epoch_accuracy.append(tf.reduce_mean(train_accuracy))
+
+        if (epoch != 0) and ((epoch + 1) % 50 == 0):
+            print(
+                "epoch {}/{} : mean loss = {}, mean accuracy = {}".format(
+                    epoch + 1,
+                    epochs,
+                    tf.reduce_mean(train_loss),
+                    tf.reduce_mean(train_accuracy),
+                )
+            )
+    return epoch_accuracy, epoch_loss
