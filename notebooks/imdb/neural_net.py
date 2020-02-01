db4c8a0a41060d6e2b2530c0d78fc481b4c136d2
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-02-02 00:35:47 +0800

    chore: add neural net for text classification

---
+++ notebooks/imdb/neural_net.py
@@ -0,0 +1,53 @@
+# Copyright 2019 Abien Fred Agarap
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""TensorFlow 2.0 implementation of neural network with text embeddings layer"""
+import tensorflow as tf
+
+__author__ = "Abien Fred Agarap"
+__version__ = "1.0.0"
+
+
+class NeuralNet(tf.keras.Model):
+    def __init__(self, **kwargs):
+        super(NeuralNet, self).__init__()
+        self.embedding_layer = tf.keras.layers.Embedding(
+            input_dim=kwargs['vocabulary_size'],
+            output_dim=kwargs['max_length'],
+            input_length=kwargs['max_length'],
+            embeddings_initializer=kwargs['embedding_initializer'],
+            trainable=False
+        )
+        self.flatten = tf.keras.layers.Flatten()
+        self.hidden_layer_1 = tf.keras.layers.Dense(
+            units=kwargs['units'][0],
+            activation=kwargs['activation'],
+            kernel_initializer=kwargs['initializer']
+        )
+        self.hidden_layer_2 = tf.keras.layers.Dense(
+            units=kwargs['units'][1],
+            activation=kwargs['activation'],
+            kernel_initializer=kwargs['initializer']
+        )
+        self.output_layer = tf.keras.layers.Dense(
+            units=kwargs['num_classes'], activation=tf.nn.sigmoid
+        )
+
+    @tf.function
+    def call(self, features):
+        embedding = self.embedding_layer(features)
+        embedding = self.flatten(embedding)
+        activation = self.hidden_layer_1(embedding)
+        activation = self.hidden_layer_2(activation)
+        output = self.output_layer(activation)
+        return output
